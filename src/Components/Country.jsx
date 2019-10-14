08999bddb1c30b305cf9e2768e759c5755ae0641
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-10-13 20:32:31 -0400

    Create Country component for rendering data on individual nations

---
+++ src/Components/Country.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import CountryData from './CountryData';
+import { checkHidden } from './utility';
+
+class Country extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hidden: true,
+        }
+
+        this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+
+    toggleDisplay(e) {
+        e.stopPropagation();
+        const currentState = this.state.hidden;
+        this.setState({ hidden: !currentState });
+    }
+
+    render() {
+        const data = this.props.data;
+        return(
+            <li
+                className={`country ${checkHidden(this.props.hidden)}`}
+                onClick={this.toggleDisplay}>
+                <h3 className="country-title">{data.name}</h3>
+                <CountryData data={data} hidden={this.state.hidden} />
+            </li>
+
+        );
+    }
+}
+
+export default Country;
