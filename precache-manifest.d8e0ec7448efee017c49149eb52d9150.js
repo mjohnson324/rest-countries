2f84e6ee9a8fbbf32997af10362d7b804865bd0b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-10-17 01:26:24 -0400

    Updates

---
+++ precache-manifest.d8e0ec7448efee017c49149eb52d9150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2005b8fe426080a909177b8ce8052e8",
+    "url": "/rest-countries/index.html"
+  },
+  {
+    "revision": "04fe524d06fdd8dcdab7",
+    "url": "/rest-countries/static/css/main.924cf738.chunk.css"
+  },
+  {
+    "revision": "f27929d74e3715d22bb0",
+    "url": "/rest-countries/static/js/2.a5d0a524.chunk.js"
+  },
+  {
+    "revision": "04fe524d06fdd8dcdab7",
+    "url": "/rest-countries/static/js/main.db7da407.chunk.js"
+  },
+  {
+    "revision": "b7e44a7028b24b0a3b83",
+    "url": "/rest-countries/static/js/runtime-main.302f3ef1.js"
+  }
+]);
